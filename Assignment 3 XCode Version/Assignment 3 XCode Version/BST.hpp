1d0d5aa7d8a00b6b064728150493edb6a6c59b64
Wang Yanjie <wang_yanjie@MacBook-Air-2.local>
2024-10-03 21:52:48 +0800

    before insert

--- Assignment 3 XCode Version/Assignment 3 XCode Version/BST.hpp
+++ Assignment 3 XCode Version/Assignment 3 XCode Version/BST.hpp
@@ -123,31 +123,40 @@ template <typename T> Tree<T>::Tree() {
 
 // Destructor
 template <typename T> Tree<T>::~Tree() {
+    deleteTree(m_root);
   // TODO: Implement this method
 }
+
+template <typename T> void deleteTree(Node<T>* root) {
+    if (root == NULL) return;
+    
+    deleteTree(root->left);
+    deleteTree(root->right);
+    delete root;
+};
 
 // Returns a pointer to the root
 template <typename T> Node<T> *Tree<T>::root() {
   // TODO: Implement this method
-  return nullptr;
+    return m_root;
 }
 
 // Checks whether the tree is empty
 template <typename T> bool Tree<T>::empty() const {
   // TODO: Implement this method
-  return true;
+    return m_size == 0;
 }
 
 // Returns the number of elements
 template <typename T> size_t Tree<T>::size() const {
   // TODO: Implement this method
-  return 0;
+    return m_size;
 }
 
 // Returns the height of the tree
 template <typename T> int Tree<T>::height() const {
   // TODO: Implement this method
-  return 0;
+    return m_root->height;
 }
 
 // Inserts an element
@@ -158,25 +167,59 @@ template <typename T> void Tree<T>::insert(T element) {
 // Checks whether the container contains the specified element
 template <typename T> bool Tree<T>::contains(T element) const {
   // TODO: Implement this method
-  return false;
+    return helperContains(element, m_root);
+}
+
+template <typename T> bool helperContains(T element, Node<T>* root) {
+    if (!root) return false;
+    if (element == root->element) return true;
+    if (element > root->element) return helperContains(element, root->right);
+    if (element < root->element) return helperContains(element, root->left);
+    
+    return false;
 }
 
 // Returns the maximum element
 template <typename T> T Tree<T>::max() const {
+    if (!m_root) return NULL;
+    
+    Node<T>* cur = m_root;
+    while (cur->right != NULL) cur = cur->right;
+    
+    return cur->element;
   // TODO: Implement this method
-  throw std::runtime_error("not implemented");
 }
 
 // Returns the minimum element
 template <typename T> T Tree<T>::min() const {
+    if (!m_root) return NULL;
+    
+    Node<T>* cur = m_root;
+    while (cur->left != NULL) cur = cur->left;
+    
+    return cur->element;
   // TODO: Implement this method
-  throw std::runtime_error("not implemented");
 }
 
 // Returns the successor of the specified element
 template <typename T> T Tree<T>::successor(T element) {
   // TODO: Implement this method
-  throw std::runtime_error("not implemented");
+    if (!contains(element)) throw std::runtime_error("not contains");
+    Node<T>* cur = helperSuccessor(element, m_root);
+    if (!cur->right) return NULL;
+    cur = cur->right;
+    while (!cur->left) cur = cur->left;
+    
+    return cur->element;
+}
+
+template <typename T> Node<T>* helperSuccessor(T element, Node<T>* root) {
+    if (!root) return NULL;
+    if (element == root->element) return root;
+    if (element > root->element) return helperSuccessor(element, root->right);
+    if (element < root->element) return helperSuccessor(element, root->left);
+    
+    return NULL;
 }
 
 template <typename T>
@@ -195,15 +238,35 @@ string Tree<T>::pre_order() {
 }
 
 template <typename T>
+string _in_order(Node<T> *node) {
+  return (node->left == nullptr ? "" : " " + _pre_order(node->left))
+    + my_to_string(node->element)
+    + (node->right == nullptr ? "" : " " + _pre_order(node->right));
+}
+
+template <typename T>
 string Tree<T>::in_order() {
   // TODO: Implement this method
-  return "";
+    if (m_root == nullptr) {
+      return "";
+    }
+    return _in_order(m_root);
+}
+
+template <typename T>
+string _post_order(Node<T> *node) {
+  return (node->left == nullptr ? "" : " " + _pre_order(node->left))
+    + (node->right == nullptr ? "" : " " + _pre_order(node->right))
+    + my_to_string(node->element);
 }
 
 template <typename T>
 string Tree<T>::post_order() {
   // TODO: Implement this method
-  return "";
+    if (m_root == nullptr) {
+      return "";
+    }
+    return _post_order(m_root);
 }
 
 #endif
