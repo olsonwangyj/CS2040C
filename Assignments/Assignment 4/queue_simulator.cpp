98477f7fff6ffd02dab8dfd274ac74fe53ea7399
Yanjie Wang <olsonwangyj@gmail.com>
2024-10-21 17:49:33 +0800

    finish heap

---
+++ Assignments/Assignment 4/queue_simulator.cpp
@@ -0,0 +1,7 @@
+#include "queue_simulator.h"
+
+vector<Customer> QueueSimulator::simulateQueue(
+    const vector<Customer>& customers) {
+  // TODO: Implement this function.
+  return customers;
+}
